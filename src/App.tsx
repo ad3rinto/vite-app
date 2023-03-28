ad1ce37404f8dc60b3662cc4863903f616193d99
ad3rinto <aad3rinto@gmail.com>
2023-03-28 14:13:58 +0100

    remove files not required and installed bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
       <Message />
+      <ListGroup />
     </div>
   );
 }
