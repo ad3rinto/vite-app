6fa7103eb8821c98ed5002b3981a21ba04c26617
ad3rinto <aad3rinto@gmail.com>
2023-03-29 17:24:10 +0100

    mapped through  cities list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,25 @@
 function ListGroup() {
-  return <h1>List Group</h1>;
+  const items = [
+    "New York",
+    "Paris",
+    "San Francisco",
+    "Copenhagen",
+    "Ebutte Meta",
+    "Stockport",
+    "Epetedo",
+  ];
+  return (
+    <div className="container">
+      <h1>Lists Of Cities</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListGroup;
