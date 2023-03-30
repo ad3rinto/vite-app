bf649a3005c87a74ad9a3bea3b5797874bb1f76f
ad3rinto <aad3rinto@gmail.com>
2023-03-30 17:22:09 +0100

    implement useState to select which list item to add active clssname to

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = [
     "New York",
@@ -10,15 +12,25 @@ function ListGroup() {
     "Ogbomoso",
     "Ejigbo",
   ];
+  // let selectedIndex = -1;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  const text = <p>No Items FOund</p>;
   return (
     <div className="container">
       <h1>Lists Of Cities</h1>
-      {items.length === 0 && text}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "active list-group-item"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
